f85f9fe2f152931a368e783e781df360d1b6cd26
Khemchand Prasad <kcpd.me@gmail.com>
2025-12-27 22:39:22 +0530

    ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/MANIFEST.MF"
         }
         jniLibs {
             useLegacyPackaging = false
